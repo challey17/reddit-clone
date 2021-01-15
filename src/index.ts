c98fbfb518d8fc103d1964c3f3875cc24f0e3a58
Chayce Knaub <chknaub@yahoo.com>
2021-01-15 00:47:25 -0500

    mikro-ORM set up complete, first sql tables created

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
+  //connect to DB
   const orm = await MikroORM.init(mikroConfig);
+  // run table migrations
+  await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
-  console.log("------------------sql 2--------------");
-  await orm.em.nativeInsert(Post, { title: "my first post 2" });
+  // const post = orm.em.create(Post, { title: "my first post" });
+  // await orm.em.persistAndFlush(post);
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 
 main();

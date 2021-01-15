122dfa80cdfa127e29f72fccd34268facc939c2d
Chayce Knaub <chknaub@yahoo.com>
2021-01-15 00:17:05 -0500

    mikro-orm

---
+++ dist/mikro-orm.config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Post_1 = require("./entities/Post");
+const constants_1 = require("./constants");
+exports.default = {
+    entities: [Post_1.Post],
+    dbName: "redditclone",
+    type: "postgresql",
+    debug: !constants_1.__prod__,
+};
+//# sourceMappingURL=mikro-orm.config.js.map

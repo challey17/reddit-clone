e3d33beb3d95191f58883811105d8e52224f16de
Chayce Knaub <chknaub@yahoo.com>
2021-01-16 18:52:04 -0500

    crud tests with graphql

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
@@ -6,6 +7,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   //connect to DB
@@ -17,9 +19,11 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    // context is a special value accessible to all resolvers
+    context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
@@ -29,4 +33,6 @@ const main = async () => {
   });
 };
 
-main();
+main().catch((err) => {
+  console.error(err);
+});

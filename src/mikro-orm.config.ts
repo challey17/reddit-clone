89ac4325cc9f73289b96ae5a683b99a90ed2ba27
Chayce Knaub <chknaub@yahoo.com>
2021-01-17 17:42:36 -0500

    user entity created and register resolver set up

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,12 +2,13 @@ import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files .ts or.js files
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "redditclone",
   type: "postgresql",
   debug: !__prod__,
